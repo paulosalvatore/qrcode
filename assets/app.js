f24d824c6ab436b5c26e7b0fe3ccff56cc38183b
PauloSalvatore <paulo.salvatore@hotmail.com.br>
2017-09-20 14:31:26 -0300

    Construção do Layout, reorganização de scripts

--- assets/app.js
+++ assets/app.js
@@ -1,202 +1,218 @@
-window.addEventListener('DOMContentLoaded', function () {
-    if (!('mediaDevices' in navigator &&
-        'getUserMedia' in navigator.mediaDevices &&
-        'Worker' in window)) {
-        alert('Sorry, your browser is not compatible with this app.');
-        return;
-    }
+$(function(){
+	if (!("mediaDevices" in navigator &&
+		"getUserMedia" in navigator.mediaDevices &&
+		"Worker" in window))
+	{
+		alert("Desculpe, seu navegador não é compatível com essa aplicação.");
 
-    // html elements
-    const snapshotCanvas = document.getElementById('snapshot');
-    const snapshotContext = snapshotCanvas.getContext('2d');
-    const video = document.getElementById('camera');
-    const overlay = document.getElementById('snapshotLimitOverlay');
-    const flipCameraButton = document.getElementById("flipCamera");
-    const loadingElement = document.getElementById('loading');
-    const resultContainer = document.getElementById('result');
-    const resultDialog = document.querySelector('dialog');
-    const resultSearchGo = document.querySelector('dialog a.search');
+		return;
+	}
 
-    // init dialog
-    dialogPolyfill.registerDialog(resultDialog);
-    resultDialog.querySelector('button.continue').addEventListener('click', function() {
-        resultDialog.close();
-        resultContainer.innerText = "";
-        flipCameraButton.disabled = false;
-        scanCode(true);
-    });
-    new Clipboard('dialog button.copy');
+	// html elements
+	const snapshotCanvas = document.getElementById("snapshot");
+	const snapshotContext = snapshotCanvas.getContext("2d");
+	const video = document.getElementById("camera");
+	const overlay = document.getElementById("snapshotLimitOverlay");
+	const flipCameraButton = document.getElementById("flipCamera");
+	const loadingElement = document.getElementById("loading");
+	const resultContainer = document.getElementById("result");
+	const resultDialog = document.querySelector("dialog");
+	const resultSearchGo = document.querySelector("dialog a.search");
 
-    // init QRCode Web Worker
-    const qrcodeWorker = new Worker("assets/qrcode_worker.js");
-    qrcodeWorker.postMessage({cmd: 'init'});
-    qrcodeWorker.addEventListener('message', showResult);
+	// init dialog
+	dialogPolyfill.registerDialog(resultDialog);
+	resultDialog.querySelector("button.continue").addEventListener("click", function() {
+		resultDialog.close();
+		resultContainer.innerText = "";
+		flipCameraButton.disabled = false;
+		scanCode(true);
+	});
+	new Clipboard("dialog button.copy");
 
-    let snapshotSquare;
-    function calculateSquare() {
-        // get square of snapshot in the video
-        let snapshotSize = overlay.offsetWidth;
-        snapshotSquare = {
-            'x': ~~((video.videoWidth - snapshotSize)/2),
-            'y': ~~((video.videoHeight - snapshotSize)/2),
-            'size': ~~(snapshotSize)
-        };
+	// init QRCode Web Worker
+	const qrcodeWorker = new Worker("assets/qrcode_worker.js");
+	qrcodeWorker.postMessage({cmd: "init"});
+	qrcodeWorker.addEventListener("message", showResult);
 
-        snapshotCanvas.width = snapshotSquare.size;
-        snapshotCanvas.height = snapshotSquare.size;
-    }
+	var snapshotSquare;
+	function calculateSquare() {
+		// get square of snapshot in the video
+		var snapshotSize = overlay.offsetWidth;
+		snapshotSquare = {
+			"x": ~~((video.videoWidth - snapshotSize)/2),
+			"y": ~~((video.videoHeight - snapshotSize)/2),
+			"size": ~~(snapshotSize)
+		};
 
-    function scanCode(wasSuccess) {
-        setTimeout(function() {
-            if (flipCameraButton.disabled) {
-                // terminate this loop
-                loadingElement.style.display = "none";
-                return;
-            }
+		snapshotCanvas.width = snapshotSquare.size;
+		snapshotCanvas.height = snapshotSquare.size;
+	}
 
-            // show loading
-            loadingElement.style.display = "block";
+	function scanCode(wasSuccess) {
+		setTimeout(function() {
+			if (flipCameraButton.disabled) {
+				// terminate this loop
+				loadingElement.style.display = "none";
+				return;
+			}
 
-            // capture current snapshot
-            snapshotContext.drawImage(video, snapshotSquare.x, snapshotSquare.y, snapshotSquare.size, snapshotSquare.size, 0, 0, snapshotSquare.size, snapshotSquare.size);
-            const imageData = snapshotContext.getImageData(0, 0, snapshotSquare.size, snapshotSquare.size);
+			// show loading
+			loadingElement.style.display = "block";
 
-            // scan for QRCode
-            qrcodeWorker.postMessage({
-                cmd: 'process',
-                width: snapshotSquare.size,
-                height: snapshotSquare.size,
-                imageData: imageData
-            });
-        }, wasSuccess ? 2000 : 120);
-    }
+			// capture current snapshot
+			snapshotContext.drawImage(video, snapshotSquare.x, snapshotSquare.y, snapshotSquare.size, snapshotSquare.size, 0, 0, snapshotSquare.size, snapshotSquare.size);
+			const imageData = snapshotContext.getImageData(0, 0, snapshotSquare.size, snapshotSquare.size);
 
-    function showResult (e) {
-        const resultData = e.data;
+			// scan for QRCode
+			qrcodeWorker.postMessage({
+				cmd: "process",
+				width: snapshotSquare.size,
+				height: snapshotSquare.size,
+				imageData: imageData
+			});
+		}, wasSuccess ? 2000 : 120);
+	}
 
-        // open a dialog with the result if found
-        if (resultData !== false) {
-            navigator.vibrate(200);
-            disableUI();
+	function showResult (e) {
+		const resultData = e.data;
 
-            try {
-                url = new URL(resultData);
-                let linkToResult = document.createElement('a');
-                linkToResult.href = url;
-                linkToResult.innerText = resultData;
-                resultContainer.appendChild(linkToResult);
+		// open a dialog with the result if found
+		if (resultData !== false) {
+			navigator.vibrate(200);
+			disableUI();
 
-                resultSearchGo.href = url;
-                resultSearchGo.innerText = "Go";
-            } catch (e) {
-                resultContainer.innerText = resultData;
+			try {
+				url = new URL(resultData);
+				var linkToResult = document.createElement("a");
+				linkToResult.href = url;
+				linkToResult.innerText = resultData;
+				resultContainer.appendChild(linkToResult);
 
-                resultSearchGo.href = "https://google.com/search?q=" + encodeURIComponent(resultData);
-                resultSearchGo.innerText = "Search";
-            }
+				resultSearchGo.href = url;
+				resultSearchGo.innerText = "Go";
+			} catch (e) {
+				resultContainer.innerText = resultData;
 
-            resultDialog.showModal();
-        } else {
-            // if not found, retry
-            scanCode();
-        }
-    }
+				resultSearchGo.href = "https://google.com/search?q=" + encodeURIComponent(resultData);
+				resultSearchGo.innerText = "Search";
+			}
 
-    function disableUI () {
-        flipCameraButton.disabled = true;
-        loadingElement.style.display = "none";
-    }
+			resultDialog.showModal();
+		} else {
+			// if not found, retry
+			scanCode();
+		}
+	}
 
-    // init video stream
-    let currentDeviceId;
-    function initVideoStream () {
-        let config = {
-            audio: false,
-            video: {}
-        };
-        config.video = currentDeviceId ? {deviceId: currentDeviceId} : {facingMode: "environment"};
+	function disableUI () {
+		flipCameraButton.disabled = true;
+		loadingElement.style.display = "none";
+	}
 
-        stopStream();
+	// init video stream
+	var currentDeviceId;
+	function initVideoStream () {
+		var config = {
+			audio: false,
+			video: {}
+		};
+		config.video = currentDeviceId ? {deviceId: currentDeviceId} : {facingMode: "environment"};
 
-        navigator.mediaDevices.getUserMedia(config).then(function (stream) {
-            document.getElementById('about').style.display = 'none';
+		stopStream();
 
-            video.srcObject = stream;
-            video.oncanplay = function() {
-                flipCameraButton.disabled = false;
-                calculateSquare();
-                scanCode();
-            };
-        }).catch(function (error) {
-            alert(error.name + ": " + error.message);
-        });
-    }
-    initVideoStream();
+		navigator.mediaDevices.getUserMedia(config).then(function (stream) {
+			document.getElementById("about").style.display = "none";
 
-    function stopStream() {
-        disableUI();
+			video.srcObject = stream;
+			video.oncanplay = function() {
+				flipCameraButton.disabled = false;
+				calculateSquare();
+				scanCode();
+			};
+		}).catch(function (error) {
+			alert(error.name + ": " + error.message);
+		});
+	}
 
-        if (video.srcObject) {
-            video.srcObject.getTracks()[0].stop();
-        }
-    }
+	function stopStream()
+	{
+		disableUI();
 
-    // listen for optimizedResize
-    window.addEventListener("optimizedResize", calculateSquare);
+		if (video.srcObject)
+			video.srcObject.getTracks()[0].stop();
+	}
 
-    // add flip camera button if necessary
-    navigator.mediaDevices.enumerateDevices()
-    .then(function(devices) {
-        devices = devices.filter(function (device) {
-            return device.kind === 'videoinput';
-        });
+	// listen for optimizedResize
+	window.addEventListener("optimizedResize", calculateSquare);
 
-        if (devices.length > 1) {
-            // add a flip camera button
-            flipCameraButton.style.display = "block";
+	// add flip camera button if necessary
+	navigator.mediaDevices.enumerateDevices()
+	.then(function(devices){
+		devices = devices.filter(function(device){
+			return device.kind === "videoinput";
+		});
 
-            currentDeviceId = devices[0].deviceId; // no way to know current MediaStream's device id so arbitrarily choose the first
+		if (devices.length > 1)
+		{
+			// add a flip camera button
+			flipCameraButton.style.display = "block";
 
-            flipCameraButton.addEventListener('click', function() {
-                let targetDevice;
-                for (let i = 0; i < devices.length; i++) {
-                    if (devices[i].deviceId === currentDeviceId) {
-                        targetDevice = (i + 1 < devices.length) ? devices[i+1] : devices[0];
-                        break;
-                    }
-                }
-                currentDeviceId = targetDevice.deviceId;
+			currentDeviceId = devices[0].deviceId; // no way to know current MediaStream's device id so arbitrarily choose the first
 
-                initVideoStream();
-            });
-        }
-    });
+			flipCameraButton.addEventListener("click", function(){
+				var targetDevice;
+				for (var i = 0; i < devices.length; i++)
+				{
+					if (devices[i].deviceId === currentDeviceId)
+					{
+						targetDevice = (i + 1 < devices.length) ? devices[i+1] : devices[0];
+						break;
+					}
+				}
 
-    document.addEventListener("visibilitychange", function() {
-        if (document.hidden) {
-            stopStream();
-        } else {
-            initVideoStream();
-        }
-    });
+				currentDeviceId = targetDevice.deviceId;
+
+				initVideoStream();
+			});
+		}
+	});
+
+	document.addEventListener("visibilitychange", function(){
+		if (document.hidden)
+			stopStream();
+		else
+			initVideoStream();
+	});
+
+	$("#iniciarCamera").click(function(){
+		initVideoStream();
+	});
 });
 
-// listen for resize event
-(function() {
-    let throttle = function(type, name, obj) {
-        obj = obj || window;
-        let running = false;
-        let func = function() {
-            if (running) { return; }
-            running = true;
-            requestAnimationFrame(function() {
-                obj.dispatchEvent(new CustomEvent(name));
-                running = false;
-            });
-        };
-        obj.addEventListener(type, func);
-    };
 
-    /* init - you can init any event */
-    throttle("resize", "optimizedResize");
+// listen for resize event
+(function(){
+	var throttle = function(type, name, obj){
+		obj = obj || window;
+
+		var running = false;
+
+		var func = function(){
+			if (running)
+				return;
+
+			running = true;
+
+			requestAnimationFrame(function(){
+				obj.dispatchEvent(new CustomEvent(name));
+				running = false;
+			});
+		};
+
+		obj.addEventListener(type, func);
+	};
+
+	/* init - you can init any event */
+	throttle("resize", "optimizedResize");
 })();
+
